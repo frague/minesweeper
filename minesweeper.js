3bfcf5615545e357f68f4792b39ba6095b28a896
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2014-08-30 23:12:26 +0400

    Implements basic gameplay without winning checks

--- minesweeper.js
+++ minesweeper.js
@@ -14,7 +14,7 @@ function play() {
 
 function minesweeper(size) {
     this.size = size;
-    this.cells = {};
+    this.cells = new cells();
     this.ended = false;
 
     this.id = function(x, y) {return x + ":" + y}
@@ -26,33 +26,64 @@ function minesweeper(size) {
             for (var x = 0, tr = $("field").append("tr", true); x < size; x++) tr.append(new cell(x, y, this));
 
         // Set the bombs
-        for (var i = 0, a = x * y / 3; i < a; i++) {
-            var id = Math.round(size * Math.random()) + ":" + Math.round(size * Math.random()), c = this.cells[id];
-            console.log(id, c);
+        for (var i = 0, a = x * y / 7; i < a; i++) {
+            var c = this.cells.get(Math.round(size * Math.random()), Math.round(size * Math.random()));
             if (!c || c.has_bomb) {
                 i--;
                 continue;
             }
+
             c.has_bomb = true;
-            //c.setState("bomb");
-            this.ended = false;
-            $("field").removeClass("exploded");
+            for (var x = -1; x < 2; x++)
+                for (var y = -1; y < 2; y++) {
+                    var c1 = this.cells.get(c.x + x, c.y + y);
+                    if (!c1) continue;
+                    c1.bombs_around++;
+                }
         }
+
+        $("field").removeClass("exploded");
+        this.ended = false;
     }
 
     this.end = function(exploded_cell) {
-        exploded_cell.setState("bomb");
         $("field").addClass("exploded");
         this.ended = true;
+        this.cells.each(function(i) {i.redraw()})
     }
 }
+
+function cells() {
+    this.data = {};
+
+    this.get = function(x, y) {
+        var i = this.data[x + ":" + y];
+        if (i) return i;
+        else return null;
+    }
+
+    this.put = function(c) {
+        this.data[c.x + ":" + c.y] = c;
+    }
+
+    this.each = function(f) {
+        for (k in this.data) f(this.data[k]);
+    }
+}
+
 
 function cell(x, y, game) {
     this.x = x;
     this.y = y;
-    this.game = game;
-    this.has_bomb = false;
     this.id = x + ":" + y;
+    this.game = game;
+
+    this.has_bomb = false;
+    this.opened = false;
+    this.discovered = false;
+    this.flagged = false;
+
+    this.bombs_around = 0;
     this.element = document.createElement("td");
     this.element.id = this.id;
     this.element.cell = this;
@@ -60,27 +91,42 @@ function cell(x, y, game) {
         this.cell.stepped();
     }
     this.element.oncontextmenu = function() {
-        var s = this.cell.state;
-        if (s == "opened" || this.cell.game.ended) return false;
-        this.cell.setState(s == "flag" ? "default" : "flag");
+        if (this.cell.opened || this.cell.game.ended) return false;
+        this.cell.flagged = !this.cell.flagged;
+        this.cell.redraw();
         return false;
     }
-    this.game.cells[this.id] = this;
-    this.setState("default");
+    this.game.cells.put(this);
+    this.redraw();
     return this.element;
 }
 
 var cp = cell.prototype;
 
-cp.setState = function(state) {
+cp.redraw = function() {
+    state = "default";
+    if (this.game.ended && this.has_bomb) state = "bomb";
+    else if (this.opened) state = "opened";
+    else if (this.flagged) state = "flag " + (this.discovered ? "opened" : "default");
+    else if (this.discovered) state = "discovered a" + this.bombs_around;
     this.element.className = "cell " + state;
-    this.state = state;
 }
 
 cp.stepped = function() {
     if (this.game.ended) return false;
     if (this.has_bomb) return game.end(this);
-   this.setState("opened"); 
+    this.opened = true;
+    this.redraw();
+    for (var x = -1; x < 2; x++)
+        for (var y = -1; y < 2; y++) {
+            if (!x && !y) continue;
+            var c = this.game.cells.get(this.x + x, this.y + y);
+            if (!c || c.opened || c.discovered) continue;
+            if (c.bombs_around) {
+                c.discovered = true;
+                c.redraw();
+            } else c.stepped();
+        }
 }
 
 
