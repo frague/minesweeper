3a4941a78ee823193015737a3436a54f467d2bc8
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2014-08-30 21:35:39 +0400

    Adds images for cells states; adds basic stepping logic and explosion

--- minesweeper.js
+++ minesweeper.js
@@ -1,65 +1,21 @@
-var mjq = function(input) {
-    this.element = null;
-    if (typeof input == "string") this.element = document.getElementById(input);
-    else if (typeof input == "object" && input.nodeName) this.element = input;
-    this.found = !!this.element;
-    console.log(this.found);
-    return this;
-
-}, mp = mjq.prototype, $ = function(input) {return new mjq(input)};
-
-mp.val = function() {
-    if (this.found) {
-        if (arguments.length) this.element.value = arguments[0];
-        else return this.element.value;
-    }
-}
-
-mp.disabled = function(state) {
-    if (this.found) this.element.disabled = state ? "disabled" : "";
-    return this;
-}
-
-mp.toggleClass = function(className) {
-    if (this.found) {
-        var c = this.element.className;
-        if (c.indexOf(className) < 0) this.element.className += " " + className; 
-        else this.element.className = c.replace(className, "");
-    }
-    return this;
-} 
-
-mp.clear = function() {
-    if (this.found) this.element.innerHTML = "";
-    return this;
-}
-
-mp.append = function(element, switch_to) {
-    if (this.found) {
-        if (typeof element == "string") element = document.createElement(element);
-        this.element.appendChild(element);
-        if (switch_to) this.element = element;
-    }
-    return this;
-}
-//------------------------------------------------------------------------------------
+var game;
 
 function validate(value) {
     var v = Math.round(value);
     $("btn_start").disabled(value != v || v < 3 || v > 20); 
-    console.log(1);
 }
 
 function play() {
-    $("welcome").toggleClass("hidden");
-    $("playground").toggleClass("hidden");
-    var game = new minesweeper(Math.round($("size").val()));
+    $("welcome").addClass("hidden");
+    $("playground").removeClass("hidden");
+    game = new minesweeper(Math.round($("size").val()));
     game.run();
 }
 
 function minesweeper(size) {
     this.size = size;
     this.cells = {};
+    this.ended = false;
 
     this.id = function(x, y) {return x + ":" + y}
 
@@ -67,15 +23,35 @@ function minesweeper(size) {
         // Generate field
         $("field").clear();
         for (var y = 0; y < this.size; y++)
-            for (var x = 0, tr = $("field").append("tr", true); x < size; x++) tr.append(new cell(x, y, this.cells));
+            for (var x = 0, tr = $("field").append("tr", true); x < size; x++) tr.append(new cell(x, y, this));
+
+        // Set the bombs
+        for (var i = 0, a = x * y / 3; i < a; i++) {
+            var id = Math.round(size * Math.random()) + ":" + Math.round(size * Math.random()), c = this.cells[id];
+            console.log(id, c);
+            if (!c || c.has_bomb) {
+                i--;
+                continue;
+            }
+            c.has_bomb = true;
+            //c.setState("bomb");
+            this.ended = false;
+            $("field").removeClass("exploded");
+        }
+    }
+
+    this.end = function(exploded_cell) {
+        exploded_cell.setState("bomb");
+        $("field").addClass("exploded");
+        this.ended = true;
     }
 }
 
-function cell(x, y, cells) {
+function cell(x, y, game) {
     this.x = x;
     this.y = y;
-    this.state = "default";
-    this.cells = cells;
+    this.game = game;
+    this.has_bomb = false;
     this.id = x + ":" + y;
     this.element = document.createElement("td");
     this.element.id = this.id;
@@ -84,13 +60,27 @@ function cell(x, y, cells) {
         this.cell.stepped();
     }
     this.element.oncontextmenu = function() {
-        console.log(this.cell.x, this.cell.y);
+        var s = this.cell.state;
+        if (s == "opened" || this.cell.game.ended) return false;
+        this.cell.setState(s == "flag" ? "default" : "flag");
         return false;
     }
-    this.cells[this.id] = this;
+    this.game.cells[this.id] = this;
+    this.setState("default");
     return this.element;
 }
 
-cell.prototype.stepped = function() {
-    
+var cp = cell.prototype;
+
+cp.setState = function(state) {
+    this.element.className = "cell " + state;
+    this.state = state;
 }
+
+cp.stepped = function() {
+    if (this.game.ended) return false;
+    if (this.has_bomb) return game.end(this);
+   this.setState("opened"); 
+}
+
+
---
+++ mjs.js
@@ -0,0 +1,47 @@
+var mjq = function(input) {
+    this.element = document.createElement("x");
+    if (typeof input == "string") this.element = document.getElementById(input);
+    else if (input.nodeName) this.element = input;
+    return this;
+
+}, mp = mjq.prototype, $ = function(input) {return new mjq(input)};
+
+mp.val = function() {
+    if (arguments.length) this.element.value = arguments[0];
+    else return this.element.value;
+}
+
+mp.disabled = function(state) {
+    this.element.disabled = state ? "disabled" : "";
+    return this;
+}
+
+mp.addClass = function(className) {
+    var c = this.element.className;
+    if (c.indexOf(className) < 0) this.element.className += " " + className; 
+    return this;
+} 
+
+mp.removeClass = function(className) {
+    var c = this.element.className;
+    if (c.indexOf(className) >= 0) this.element.className = c.replace(className, "");
+    return this;
+} 
+
+mp.toggleClass = function(className) {
+    var c = this.element.className;
+    if (c.indexOf(className) < 0) return this.addClass(className); 
+    else return this.removeClass(className);
+} 
+
+mp.clear = function() {
+    this.element.innerHTML = "";
+    return this;
+}
+
+mp.append = function(element, switch_to) {
+    if (typeof element == "string") element = document.createElement(element);
+    this.element.appendChild(element);
+    if (switch_to) this.element = element;
+    return this;
+}
